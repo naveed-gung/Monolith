1c1c699e171761a79b6d0b0404f25b0e8c00fd2f
Navid <naveedsohailg@gmail.com>
2026-06-01 12:57:48 +0300

    add lib/main.dart

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:just_audio_background/just_audio_background.dart';
+
+import 'src/app/monolith_app.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await JustAudioBackground.init(
+    androidNotificationChannelId: 'com.example.monolith.playback',
+    androidNotificationChannelName: 'Monolith playback',
+    androidNotificationOngoing: true,
+  );
+  runApp(const MonolithApp());
+}
